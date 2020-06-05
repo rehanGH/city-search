80e9b5a10b8554957670e12541d550eda9c16050
Rehan Khan <rehangh8924@gmail.com>
2020-06-04 21:36:21 -0400

    added basic layout of app

--- src/Components/Citysearch.js
+++ src/Components/Citysearch.js
@@ -1,11 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
 
 export default class Citysearch extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+    constructor(props){
+        super(props);
+        this.state ={
+            city: null,
+            newCity: []
+        };
+        this.handleCityChange = this.handleCityChange.bind(this);
     }
-}
+
+    handleCityChange(events){
+        this.setState({
+            city: events.target.value
+        });
+        const url = `http://ctp-zip-api.herokuapp.com/city/`;
+        axios.get(url + events.target.value)
+        .then((response) => {
+            const data = response.data;
+            this.setState({newCity: data});
+        })
+        .catch((err) => console.log(err));
+    }
+
+
+    render() {
+        let display;
+            display = (
+            <div className = "search">
+                <label htmlFor="zip">Enter a city name: </label>
+                <input placeholder="SPRINGFIELD" type="text" value={this.state.city} onChange = {this.handleCityChange} ></input>
+            </div>
+               );
+               return (
+               <div>
+               <div>
+                   {display}
+               </div>
+               <div className = "zip">
+                   {this.state.newCity.map(zip => {
+                       return (
+                       <div>
+                           {zip.LocationText}<br></br>
+                           <br></br>
+                       </div>)
+                   })
+               }
+               </div>
+               </div>
+               )
+           }
+       }
